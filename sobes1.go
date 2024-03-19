ce7824da096c7478c664ee4dac2e8e4a1f118dae
putilin.lev <vash-agent48@yandex.ru>
2024-03-19 19:36:59 +0300

    iter2 sqlx

--- sobes1.go
+++ sobes1.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
@@ -11,42 +12,7 @@ import (
 	"github.com/lib/pq"
 )
 
-func printItemsByShelves(db *sqlx.DB, orderID int, shelf string, first bool) error {
-	// Подготовка запроса
-	stmt, err := db.Preparex(`
-		SELECT 
-			ms.name AS main_shelf,
-			oi.order_id,
-			p.name AS product_name,
-			COUNT(oi.id) AS total_items,
-			ARRAY(
-				SELECT DISTINCT asl.name
-				FROM AdditionalShelves asl
-				WHERE asl.id IN (
-					SELECT psr2.add_shelf_id
-					FROM ProductShelfRelations psr2
-					WHERE psr2.product_id = p.id
-				)
-			) AS additional_shelves
-		FROM 
-			OrderItems oi,
-			Products p,
-			MainShelves ms,
-			ProductShelfRelations psr
-		WHERE 
-			oi.product_id = p.id
-			AND p.id = psr.product_id
-			AND psr.main_shelf_id = ms.id
-			AND oi.order_id = $1
-			AND ms.name = $2
-		GROUP BY 
-			ms.name, oi.order_id, p.name, p.id;
-	`)
-	if err != nil {
-		return err
-	}
-	defer stmt.Close()
-
+func printItemsByShelves(stmt *sql.Stmt, orderID int, shelf string, first bool) error {
 	// Выполнение подготовленного запроса с передачей параметров
 	rows, err := stmt.Query(orderID, shelf)
 	if err != nil {
@@ -96,6 +62,41 @@ func main() {
 	}
 	defer db.Close()
 
+	// Подготовка запроса
+	stmt, err := db.Prepare(`
+		SELECT 
+			ms.name AS main_shelf,
+			oi.order_id,
+			p.name AS product_name,
+			COUNT(oi.id) AS total_items,
+			ARRAY(
+				SELECT DISTINCT asl.name
+				FROM AdditionalShelves asl
+				WHERE asl.id IN (
+					SELECT psr2.add_shelf_id
+					FROM ProductShelfRelations psr2
+					WHERE psr2.product_id = p.id
+				)
+			) AS additional_shelves
+		FROM 
+			OrderItems oi,
+			Products p,
+			MainShelves ms,
+			ProductShelfRelations psr
+		WHERE 
+			oi.product_id = p.id
+			AND p.id = psr.product_id
+			AND psr.main_shelf_id = ms.id
+			AND oi.order_id = $1
+			AND ms.name = $2
+		GROUP BY 
+			ms.name, oi.order_id, p.name, p.id;
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
 	// Добавление индекса на столбец order_id в таблице OrderItems
 	_, err = db.Exec("CREATE INDEX IF NOT EXISTS idx_order_id ON OrderItems (order_id)")
 	if err != nil {
@@ -122,7 +123,7 @@ func main() {
 			}
 
 			// Вызов функции для вывода информации по заказу для текущего основного стеллажа
-			err = printItemsByShelves(db, orderID, shelf, first)
+			err = printItemsByShelves(stmt, orderID, shelf, first)
 			if err != nil {
 				fmt.Printf("Ошибка при обработке заказа %d для стеллажа %s: %v\n", orderID, shelf, err)
 			}
